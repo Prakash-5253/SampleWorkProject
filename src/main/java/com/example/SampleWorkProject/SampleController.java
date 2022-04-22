4324ee133ed14d22060b61d5b33de99d0351d685
Prakash-5253 <gnana.prakash@thecareerlabs.com>
2022-04-22 13:19:52 +0530

    project

---
+++ src/main/java/com/example/SampleWorkProject/SampleController.java
@@ -0,0 +1,14 @@
+package com.example.SampleWorkProject;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.print.DocFlavor;
+
+@RestController
+public class SampleController {
+
+    public String get(){
+        return "String";
+
+    }
+}
---
+++ src/main/java/com/example/SampleWorkProject/SampleWorkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SampleWorkProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWorkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWorkProjectApplication.class, args);
+	}
+
+}
